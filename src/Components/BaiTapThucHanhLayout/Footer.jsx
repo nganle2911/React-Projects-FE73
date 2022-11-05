60568281ec837e80285e8129c60bfa23d19deab8
Ngan LE <ngan.ltkn2911@gmail.com>
2022-11-05 16:55:08 +0700

    Add footer

---
+++ src/Components/BaiTapThucHanhLayout/Footer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+  render() {
+    return (
+        <div className='footer bg-dark'>
+            <div className='container'>
+                <p>Copyright © Your Website 2022</p>
+            </div>
+        </div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from './Components/BaiTapThucHanhLayout/Header';
+import Footer from './Components/BaiTapThucHanhLayout/Footer';
 
 // SASS
 import "./index.scss";
@@ -11,5 +12,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
     <Header />
+    <Footer />
   </>
 );
