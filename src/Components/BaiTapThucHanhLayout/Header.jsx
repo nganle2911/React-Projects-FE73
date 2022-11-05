1268baa315844d0937f1d98d86248d1ed03d94bb
Ngan LE <ngan.ltkn2911@gmail.com>
2022-11-05 16:46:46 +0700

    Style for header

--- src/Components/BaiTapThucHanhLayout/Header.jsx
+++ src/Components/BaiTapThucHanhLayout/Header.jsx
@@ -3,24 +3,43 @@ import React, { Component } from 'react'
 export default class Header extends Component {
   render() {
     return (
-        <div className='bg-dark text-white'>
-            <div className='container d-flex justify-content-between'>
-                <div className='header-left'>
-                    <p>Start Bootstrap</p>
-                </div>
-                <ul className='header-right'>
-                    <li>
-                        <a>Home</a>
-                    </li>
-                    <li>
-                        <a>About</a>
-                    </li>
-                    <li>
-                        <a>Contact</a>
-                    </li>
-                </ul>
-            </div>
+      <nav class="navbar navbar-expand-lg bg-dark">
+        <div class="container px-lg-5">
+          <a class="navbar-brand" href="#">
+            Start Bootstrap
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  About
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
+      </nav>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from './Components/BaiTapThucHanhLayout/Header';
 
+// SASS
+import "./index.scss";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
